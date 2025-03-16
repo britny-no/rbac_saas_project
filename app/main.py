f9b103c9694ed930e2db5ba16d72a20dca47eb40
tinker-britny <njw990@tinker.style>
2025-03-16 20:31:04 +0900

    update: global exception added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import sys
 import os
-
 from fastapi import FastAPI
-from .user.routers.user_router import router as user_router
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.exceptions import sqlalchemy_exception_handler, http_exception_handler
+from app.user.routers.user_router import router as user_router
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 
 app = FastAPI()
+
+app.add_exception_handler(SQLAlchemyError, sqlalchemy_exception_handler)
+app.add_exception_handler(HTTPException, http_exception_handler)
 
 app.include_router(user_router)
 
